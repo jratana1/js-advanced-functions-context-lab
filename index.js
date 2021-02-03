d823abd705b031207e1e51cbdef90d193654c785
John Ratana <jratana1@gmail.com>
2021-02-03 15:38:19 -0500

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,64 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+let createEmployeeRecord = function(array){
+    return {firstName: array[0],
+            familyName: array[1],
+            title: array[2],
+            payPerHour: array[3],
+            timeInEvents: [],
+            timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(array){
+    return array.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateStamp){
+    let obj = {type: "TimeIn",
+                hour: parseInt(dateStamp.split(" ")[1]),
+                date: dateStamp.split(" ")[0]
+    }
+
+    this.timeInEvents.push(obj)
+
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let obj = {type: "TimeOut",
+                hour: parseInt(dateStamp.split(" ")[1]),
+                date: dateStamp.split(" ")[0]
+    }
+
+    this.timeOutEvents.push(obj)
+
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    let work = this.timeOutEvents.find(function(e){ return e.date === dateStamp})
+
+    if (work){
+        return (work.hour - this.timeInEvents.find(function(e){return e.date === dateStamp}).hour)/100
+    }
+    else{
+    return 0
+    }
+}
+
+function wagesEarnedOnDate(dateStamp){
+    return hoursWorkedOnDate.call(this, dateStamp)*this.payPerHour
+}
+
+function calculatePayroll(array){
+    return array.reduce(function(last, current){return last + allWagesFor.call(current)}, 0)
+}
+
+function findEmployeeByFirstName(array, name){
+    return array.find(function(e){return e.firstName === name})
+}
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
